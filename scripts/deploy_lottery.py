7cd3b3015a6c1f8daa0f8ef3ab2dcdd3caa65b10
rholderfield <raholderfield@gmail.com>
2021-09-28 21:23:49 -0700

    updates

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -1,5 +1,10 @@
+from scripts.helpful_scripts import get_account, get_contract
+from brownie import Lottery
+
+
 def deploy_lottery():
-    pass
+    account = get_account()
+    lottery = Lottery.deploy(get_contract())
 
 
 def main():
