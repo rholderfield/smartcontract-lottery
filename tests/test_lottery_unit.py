1852390c6e7e6112bb7242c259d560ad77b3e8e0
rholderfield <raholderfield@gmail.com>
2021-10-01 10:45:03 -0700

    updated

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -1,15 +1,21 @@
-from brownie import Lottery, accounts, config, network
+from brownie import Lottery, config, network
+from scripts.deploy_lottery import deploy_lottery
 from web3 import Web3
+
 
 # 0.017
 # 16000000000000000
+# 16646081013147100
 
 
-def test_get_enterance_fee():
-    account = accounts[0]
-    lottery = Lottery.deploy(
-        config["networks"][network.show_active()]["eth_usd_price_feed"],
-        {"from": account},
-    )
-    assert lottery.getEnteranceFee() > Web3.toWei(0.014, "ether")
-    assert lottery.getEnteranceFee() < Web3.toWei(0.018, "ether")
+def test_get_entrance_fee():
+    # Arrange
+    lottery = deploy_lottery()
+    # Act
+    # 2,000 eth / usd
+    # usdEntryFee is 50
+    # 2000/1 == 50/x == 0.025
+    expected_entrance_fee = Web3.toWei(0.025, "ether")
+    entrance_fee = lottery.getEnteranceFee()
+    # Assert
+    assert expected_entrance_fee == entrance_fee
